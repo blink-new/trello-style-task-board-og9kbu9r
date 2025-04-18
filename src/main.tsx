468e4600b4bf620b2b4c13cec49d0a185da1b780
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-04-18 00:01:08 +0000

    Add Error Boundary to Application

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import App from './App';
+import { ErrorFallback } from './components/ErrorFallback';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 );
